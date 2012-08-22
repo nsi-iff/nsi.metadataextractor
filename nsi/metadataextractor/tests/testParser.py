0ab06d74840ae5b3ba4fd2b17d561f69804d1a9b
Oswaldo F <oswluizf@gmail.com>
2012-08-22 18:32:24 -0300

    working with lowcase

--- nsi/metadataextractor/tests/testParser.py
+++ nsi/metadataextractor/tests/testParser.py
@@ -26,13 +26,13 @@ class TestParser(unittest.TestCase):
         
         metadata_hash.get("author_position") |should| equal_to (0)
         metadata_hash.get("author_sucessor") |should| equal_to (['\n'])
-        metadata_hash.get("campus_prefix") |should| equal_to (['CAMPUS', 'CAMPI'])
+        metadata_hash.get("campus_antecessor") |should| equal_to (['CAMPUS', 'CAMPI'])
         metadata_hash.get("grade_doctoral") |should| equal_to (['DOUTORADO','DOUTOR'])
         metadata_hash.get("grade_graduation") |should| equal_to (['GRADUAÇÃO', 'CURSO SUPERIOR', 'BACHARELADO', 'LICENCIATURA', 'TECNÓLOGO'])
         metadata_hash.get("grade_master_degree") |should| equal_to (['MESTRADO', 'MESTRE'])
         metadata_hash.get("grade_postdoctoral") |should| equal_to (['PÓS-DOUTORADO', 'PÓS DOUTORADO'])
         metadata_hash.get("grade_spec") |should| equal_to (['PÓS-GRADUAÇÃO', 'LATO SENSU', 'ESPECIALIZAÇÃO', 'ESPECIALISTA', 'MBA'])
-        metadata_hash.get("institution_prefix") |should| equal_to (['UNIVERSIDADE', 'INSTITUTO'])
+        metadata_hash.get("institution_antecessor") |should| equal_to (['universidade', 'instituto'])
         metadata_hash.get("page") |should| equal_to (2)
         metadata_hash.get("title_antecessor") |should| equal_to (['\n'])
         metadata_hash.get("title_sucessor") |should| equal_to (['\n'])
@@ -44,7 +44,7 @@ class TestParser(unittest.TestCase):
         metadata_hash = self.tccParse.variouspages_metadata
 
         metadata_hash.get("abstract_position") |should| equal_to (1)
-        metadata_hash.get("abstract_prefix") |should| equal_to (['RESUMO\n'])
+        metadata_hash.get("abstract_antecessor") |should| equal_to (['RESUMO\n'])
         metadata_hash.get("pages") |should| equal_to ([1,10])
 
 
