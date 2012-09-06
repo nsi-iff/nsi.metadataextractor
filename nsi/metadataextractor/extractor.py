a8412ae7b6adf34c2c41e6c8ad61a4f55194c78b
Oswaldo F <oswluizf@gmail.com>
2012-09-06 19:36:19 -0300

    now working with nltk and path params instead document names

--- nsi/metadataextractor/extractor.py
+++ nsi/metadataextractor/extractor.py
@@ -1,132 +1,125 @@
 #coding: utf-8
 from os import system, remove
-from os.path import abspath, dirname, join
+from os.path import abspath, dirname, join, basename, splitext
+from string import punctuation
+from nltk.corpus import PlaintextCorpusReader
+from nltk.tokenize import line_tokenize, word_tokenize
 from xml_parser import Parser
 
 ROOT = abspath(dirname(__file__))
 
 class Preparator(object):
     
-    def __init__(self, doc_type, doc_name):
-        self.doc_type = doc_type
-        self.doc_name = doc_name
-        self.doc_dir = join(ROOT, 'articles', self.doc_type)
+    def __init__(self, doc_dir):
+        self.doc_dir, self.doc_ext = splitext(doc_dir)
+        self.doc_name = basename(self.doc_dir)
+        self.temp_text_doc = ('%s.txt' %self.doc_name)
+        
 
     def convert_document(self, page1, page2):
-        system("pdftotext -enc UTF-8 -f %i -l %i %s/%s.pdf %s/%s.txt"
-            %(page1, page2, self.doc_dir, self.doc_name, self.doc_dir, self.doc_name))
-        return self.open_document()
-  
-    def open_document(self):
-        self.document = open('%s/%s.txt' %(self.doc_dir, self.doc_name),'r').readlines()
-        return self.document
+        system("pdftotext -enc UTF-8 -f %i -l %i %s.pdf %s.txt"
+            %(page1, page2, self.doc_dir, self.doc_dir))
+        self.raw_text = PlaintextCorpusReader(dirname(self.doc_dir), self.temp_text_doc).raw().lower()
+        return self.raw_text
 
     def remove_converted_document(self):
-        remove('%s/%s.txt' %(self.doc_dir, self.doc_name))
+        remove('%s.txt' %self.doc_dir)
 
     def parse_corpus(self, corpus_type):
-        self.solid_corpus = []
-        self.corpus_dir = join(ROOT, 'corpus', '%s.txt' % corpus_type)
-        corpus = open(self.corpus_dir, 'r').readlines()
-        for item in corpus:
-            self.solid_corpus.append(item.strip())
-        return self.solid_corpus
-
-    def clean_document_list(self, document_list):
-        self.bigstring = ''
-        for line in document_list:
-            low_line = line.decode('utf-8').lower().encode('utf-8')
-            self.bigstring += low_line.replace('\n', ' ').replace(',', '').replace('.', '')
-        return self.bigstring.split()
+        self.corpus_type = '%s.txt' %corpus_type
+        self.corpus_dir = join(ROOT, 'corpus')
+        self.corpus = line_tokenize(PlaintextCorpusReader(self.corpus_dir, self.corpus_type).raw().lower())
+        if corpus_type == 'institution':
+            for line in range(len(self.corpus)):
+                self.corpus[line] = self.corpus[line].split(',')
+        return self.corpus
 
 
 class TccExtractor(object):
 
-    def __init__(self, doc_name):
+    def __init__(self, doc_dir):
         parse = Parser(join(ROOT, 'templates', 'tcc.xml'))
         self._template_metadata = parse.xml_template_metadata()
         page = self._template_metadata['page']
         pages = self._template_metadata['pages']
-        self._preparator = Preparator('obtencaograu', doc_name)
-        self._onepage_doc = self._preparator.convert_document(page, page)
-        self._variouspages_doc = self._preparator.convert_document(pages[0], pages[1])
+        self._preparator = Preparator(doc_dir)
+        self._raw_onepage_doc = self._preparator.convert_document(page, page)
+        self._linetokenized_onepage_raw_doc = open('%s.txt' %self._preparator.doc_dir).readlines()
+        self._raw_variouspages_doc = self._preparator.convert_document(pages[0], pages[1])
+        self._linetokenized_variouspages_raw_doc = open('%s.txt' %self._preparator.doc_dir).readlines()
+        self._linetokenized_onepage_doc = line_tokenize(self._raw_onepage_doc)
+        self._wordtokenized_onepage_doc = [w for w in word_tokenize(self._raw_onepage_doc) if w not in list(punctuation)]
 
 
     def _author_metadata(self):
-        self.authors = []
+        self.authors = []        
         name_corpus = self._preparator.parse_corpus('names')
-        sucessor = self._template_metadata['author_sucessor'][0]
         residues = self._template_metadata['author_residue']
-        for line in self._onepage_doc:
-            line_mod = set(line.lower().split())
+        for line in self._linetokenized_onepage_doc:
+            line_mod = set(word_tokenize(line))
             corpus_common = bool(line_mod.intersection(name_corpus))
             residue = bool(line_mod.intersection(residues))
             if corpus_common and not residue:
-                self.authors.append(line)
+                self.authors.append(line.title())
         return self.authors
 
     def _title_metadata(self):
-        i = 0
         self.title = ''
         title_antecessor = self._template_metadata['title_antecessor'][0]
         title_sucessor = self._template_metadata['title_sucessor'][0]
         breakers = self._template_metadata['title_breaker']
-        first_line = self._onepage_doc.index(title_antecessor) + 1
-        doc_range = len(self._onepage_doc)
+        first_line = self._linetokenized_onepage_raw_doc.index(title_antecessor) + 1
+        doc_range = len(self._linetokenized_onepage_raw_doc)
         for title_type_metadata_index in range(first_line, doc_range):
-            line_mod = set(self._onepage_doc[title_type_metadata_index].lower().split())
+            line_mod = set(word_tokenize(self._linetokenized_onepage_raw_doc[title_type_metadata_index]))
             breaker = bool(line_mod.intersection(breakers))
-            if self._onepage_doc[title_type_metadata_index] != title_sucessor and not breaker:
-                self.title += self._onepage_doc[title_type_metadata_index].replace('\n',' ').strip()
+            if self._linetokenized_onepage_raw_doc[title_type_metadata_index] != title_sucessor and not breaker:
+                self.title += self._linetokenized_onepage_raw_doc[title_type_metadata_index].replace('\n',' ')
             else: break
-        return self.title
+        return self.title.strip()
 
     def _institution_metadata(self):
-        self.institution_names = []
-        self.institution_prepositions = []
         self.institution = 'Instituto Federal de Educação Ciência e Tecnologia '
-        clean_document_list = self._preparator.clean_document_list(self._onepage_doc)
-        institution_list_with_preposition = self._preparator.parse_corpus('institution')
-        for item in institution_list_with_preposition:
-            item = item.split(',')
-            self.institution_prepositions.append(item[0])
-            self.institution_names.append(item[1].decode('utf-8').lower().encode('utf-8'))
-        for name in self.institution_names:
-            name_mod = set(name.split())
-            if set(name_mod).intersection(clean_document_list) == name_mod:
-                preposition = self.institution_prepositions[self.institution_names.index(name)]
-                self.institution = self.institution + preposition + name.capitalize()
-                break
+        institution_validator = set(self._template_metadata['institution_validator'])
+        has_institution = bool(institution_validator.intersection(self._wordtokenized_onepage_doc))
+        if has_institution:
+            institution_corpus = self._preparator.parse_corpus('institution')
+            for preposition, institution in institution_corpus:
+                name_mod = set(institution.split())
+                if name_mod.intersection(self._wordtokenized_onepage_doc) == name_mod:
+                    self.institution = self.institution + preposition + institution.title()
+                    break
         return self.institution
 
     def _campus_metadata(self):
         self.campus = ''
-        clean_document_list = self._preparator.clean_document_list(self._onepage_doc)
         campus_validator = set(self._template_metadata['campus_validator'])
-        has_campus = bool(campus_validator.intersection(clean_document_list))
+        has_campus = bool(campus_validator.intersection(self._wordtokenized_onepage_doc))
         if has_campus:
             self.campus_corpus = self._preparator.parse_corpus('campus')
             for campus in self.campus_corpus:
-                campus_mod = set(campus.lower().split())
-                if set(campus_mod).intersection(clean_document_list) == campus_mod:
-                    self.campus = campus
+                campus_mod = set(campus.split())
+                if campus_mod.intersection(self._wordtokenized_onepage_doc) == campus_mod:
+                    self.campus = campus.title()
                     break
         return self.campus
         
     def _abstract_metadata(self):
         self.abstract = ''
-        self.abstract_position = 1
         abstract_antecessor = self._template_metadata['abstract_antecessor'][0]
         abstract_sucessor = self._template_metadata['abstract_sucessor'][0]
-        if abstract_antecessor not in self._variouspages_doc:
-            abstract_antecessor = '\x0c' + abstract_antecessor
-        self.abstract_position += self._variouspages_doc.index(abstract_antecessor)
-        doc_range = len(self._variouspages_doc)
-        while self._variouspages_doc[self.abstract_position] == abstract_sucessor: self.abstract_position += 1
-        while self._variouspages_doc[self.abstract_position] != abstract_sucessor:
-            self.abstract += self._variouspages_doc[self.abstract_position]
-            self.abstract_position += 1
+        if abstract_antecessor not in self._linetokenized_variouspages_raw_doc:
+            abstract_antecessor = '\x0c%s' %abstract_antecessor
+        abstract_position = self._linetokenized_variouspages_raw_doc.index(abstract_antecessor) + 1
+        doc_range = len(self._linetokenized_variouspages_raw_doc)
+        while self._linetokenized_variouspages_raw_doc[abstract_position] == abstract_sucessor:
+            abstract_position += 1
+        while self._linetokenized_variouspages_raw_doc[abstract_position] != abstract_sucessor:
+            self.abstract += self._linetokenized_variouspages_raw_doc[abstract_position]
+            abstract_position += 1
         return self.abstract
+        
+
 
     def all_metadata(self):
         metadata = {'author_metadata':      self._author_metadata(),
