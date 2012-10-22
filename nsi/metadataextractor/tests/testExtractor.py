19adfa4355bcb6d6c3f6ae452c004d647141f24b
Oswaldo F <oswluizf@gmail.com>
2012-10-22 20:37:44 -0200

    working tests for event and tcc document extraction

--- nsi/metadataextractor/tests/testExtractor.py
+++ nsi/metadataextractor/tests/testExtractor.py
@@ -1,12 +1,13 @@
 #coding: utf-8
 import sys
+import re
 from os import listdir
 from os.path import abspath, dirname, join, basename
 
 import unittest
 from should_dsl import should, should_not
 from nsi.metadataextractor.xml_parser import Parser
-from nsi.metadataextractor.extractor import Preparator, TccExtractor
+from nsi.metadataextractor.extractor import Preparator, TccExtractor, EventExtractor
 
 ROOT_PATH = abspath(dirname(__file__))
 TEMPLATES_PATH = join(ROOT_PATH, '..', 'templates')
@@ -26,17 +27,17 @@ class TestPreparation(unittest.TestCase):
 	
 	def test_pdf_to_txt_convertion(self):
 		page = self.xml_template_metadata['page']
-		self.preparator.convert_document(page, page)
+		self.preparator.pdf_to_raw_text(page, page)
 		documents = listdir(dirname(self.doc_dir))
 		self.preparator.temp_text_doc |should| be_into(documents)
 
 	def test_name_corpus_has_a_certain_quantity_of_names(self):
-		len(self.preparator.parse_corpus('names')) |should| equal_to(6286)
+		len(self.preparator.parse_corpus('names')) |should| equal_to(6289)
 
 	def test_temporary_text_files_is_being_removed(self):
 		page = self.xml_template_metadata['page']
 		documents = listdir(dirname(self.doc_dir))
-		self.preparator.convert_document(page, page)
+		self.preparator.pdf_to_raw_text(page, page)
 		self.preparator.temp_text_doc |should| be_into(documents)
 		self.preparator.remove_converted_document()
 
@@ -67,37 +68,50 @@ class TestTccExtractor(unittest.TestCase):
 		self.parse = Parser('tcc.xml')
 		self.xml_template_metadata = self.parse.xml_template_metadata()
 
-	def test_has_one_or_more_author_type_metadata_on_a_list(self):
+	def test_tcc_document_has_one_or_more_confirmed_by_corpus_author_type_metadata(self):
 		len(self.extractor._author_metadata()) |should| be_greater_than_or_equal_to(1)
 		self.extractor._author_metadata() |should_not| contain('')
 		self.preparator.remove_converted_document()
 
-	def test_has_title_type_metadata(self):
+	def test_tcc_document_has_title_type_metadata(self):
 		self.extractor._title_metadata() |should_not| equal_to('')
 		self.preparator.remove_converted_document()		
 
- 	def test_document_has_a_confirmed_by_corpus_institution_metadata(self):
+ 	def test_tcc_document_has_a_confirmed_by_corpus_institution_metadata(self):
  		self.extractor._institution_metadata() |should_not| equal_to('Instituto Federal de Educação Ciência e Tecnologia ')
  		self.preparator.remove_converted_document()
  	
- 	def test_document_has_a_confirmed_by_corpus_campus_metadata(self):
+ 	def test_tcc_document_has_a_confirmed_by_corpus_campus_metadata(self):
  		self.extractor._campus_metadata() |should_not| equal_to('')
  		self.preparator.remove_converted_document()
 
- 	def test_document_has_abstract_metadata_type(self):
- 		doc = self.extractor._linetokenized_variouspages_raw_doc
- 		abstract_antecessor = self.xml_template_metadata['abstract_antecessor'][0]
- 		if abstract_antecessor not in doc:
- 			abstract_antecessor = '\x0c%s' %abstract_antecessor
-		abstract_antecessor |should| be_into(doc)
- 		self.extractor._abstract_metadata() |should| end_with('\n')
- 		self.preparator.remove_converted_document()
+ 	def test_tcc_document_has_an_abstract_metadata_pattern_found_by_regex(self):
+ 		doc = self.extractor._clean_variouspages_doc
+ 		matches = re.search(r'resumo:* (.*?) palavr(a|as)(.|\s)chav(e|es).', doc)
+ 		matches.group() |should| start_with('resumo')
+ 		self.extractor._abstract_metadata |should_not| equal_to('')
+
+class TestEventExtractor(unittest.TestCase):
+
+	def setUp(self):
+		self.doc_dir = join(ROOT_PATH, 'testdocs', 'event', '1_pt-br.pdf')
+		self.preparator = Preparator(self.doc_dir)
+		self.extractor = EventExtractor(self.doc_dir)
+		self.parse = Parser('event.xml')
+		self.xml_template_metadata = self.parse.xml_template_metadata()
+
+	def test_event_document_has_an_abstract_metadata_pattern_found_by_regex(self):
+		doc = self.extractor._clean_onepage_doc
+ 		matches = re.search(r'resumo:* (.*?) palavr(a|as)(.|\s)chav(e|es).', doc)
+ 		matches.group() |should| start_with('resumo')
+ 		self.extractor._abstract_metadata |should_not| equal_to('')
+
+	def test_event_document_has_author_type_metadata(self):
+		self.extractor._author_metadata() |should_not| be_empty
+
+	def test_event_document_has_title_type_metadata(self):
+		self.extractor._title_metadata() |should_not| be_empty 
 
 
 if __name__ == '__main__':
-	unittest.main()
-
-	
-
-		
-		
+	unittest.main()		
--- nsi/metadataextractor/tests/testParser.py
+++ nsi/metadataextractor/tests/testParser.py
@@ -13,54 +13,34 @@ class TestParser(unittest.TestCase):
 
     def setUp(self):
         self.tccParse = Parser(join(TEMPLATE_PATH, 'tcc.xml'))
-        self.eventannalsParse = Parser(join(TEMPLATE_PATH, 'anais_evento.xml'))
+        self.eventParse = Parser(join(TEMPLATE_PATH, 'event.xml'))
 
-    def test_parser_has_a_xml_directory(self):
-        self.tccParse.file_path |should| be_like(r'.*.xml')
+    def test_parser_receive_a_xml_directory(self):
+        self.tccParse.file_path and self.eventParse.file_path |should| be_like(r'.*.xml')
 
-    def test_onepage_metadata_has_a_valid_hash(self):
-        type(self.tccParse._onepage_metadata()) |should| equal_to(dict)
+    def tes_tcc_onepage_metadata_hash_has_valid_keys_and_values(self):
+        onepage_parser = self.tccParse._onepage_metadata() 
+        "author_residue", "institution_validator", "campus_validator",
+        "grade_graduation", "grade_spec", "grade_master_degree", "grade_doctoral" 
+        "grade_postdoctoral" |should| be_into(onepage_parser.keys())
+        for key in onepage_parser.keys():
+            onepage_parser.get(key) |should_not| equal_to ([])
 
-    def test_onepage_metadata_hash_has_valid_keys_and_values(self):
-        onepage_metatada = self.tccParse._onepage_metadata()
+    def test_event_onepage_metadata_hash_has_valid_keys_and_values(self):
+        onepage_parser = self.tccParse._onepage_metadata() 
+        "author_breaker", "author_residue", "title_breaker" |should| be_into(onepage_parser.keys())
+        for key in onepage_parser.keys():
+            onepage_parser.get(key) |should_not| equal_to ([])
+
+    def test_tcc_variouspages_metadata_hash_has_valid_keys_and_values(self):
+        variouspages_parser = self.tccParse._variouspages_metadata()
+        "pages" |should| be_into(variouspages_parser.keys()) 
         
-        onepage_metatada.get("author_position") |should| equal_to (0)
-        onepage_metatada.get("author_sucessor") |should| equal_to (['\n'])
-        onepage_metatada.get("campus_validator") |should| equal_to (['campus', 'campi'])
-        onepage_metatada.get("grade_doctoral") |should| equal_to (['doutorado','doutor'])
-        onepage_metatada.get("grade_graduation") |should| equal_to (['graduação', 'curso superior', 'bacharelado', 
-            'licenciatura', 'tecnólogo'])
-        onepage_metatada.get("grade_master_degree") |should| equal_to (['mestrado', 'mestre'])
-        onepage_metatada.get("grade_postdoctoral") |should| equal_to (['pós doutorado', 'pós-doutorado'])
-        onepage_metatada.get("grade_spec") |should| equal_to (['pós-graduação','lato sensu','especialização',
-            'especialista','mba'])
-        onepage_metatada.get("institution_validator") |should| equal_to (['universidade', 'instituto'])
-        onepage_metatada.get("page") |should| equal_to (2)
-        onepage_metatada.get("title_antecessor") |should| equal_to (['\n'])
-        onepage_metatada.get("title_sucessor") |should| equal_to (['\n'])
-        onepage_metatada.get("title_breaker") |should| equal_to (['monografia'])
-        onepage_metatada.get("author_residue") |should| equal_to (['orientador', 'orientadora', 'co-orientador', 
-            'co-orientadora', 'prof.', 'profa.', 'reitora:', 'pr\xc3\xb3-reitora:', 'reitora', 'pr\xc3\xb3-reitora', 
-            'dra.', 'dr.', 'instituto', 'universidade'])
-
-    def test_variouspages_metadata_has_a_valid_hash(self):
-        type(self.tccParse._onepage_metadata()) |should| equal_to(dict)
-
-    def test_variouspages_metadata_hash_has_valid_keys_and_values(self):
-        variouspages_metatada = self.tccParse._variouspages_metadata()
-
-        variouspages_metatada.get("abstract_antecessor") |should| equal_to (['RESUMO\n'])
-        variouspages_metatada.get("abstract_sucessor") |should| equal_to (['\n'])
-        variouspages_metatada.get("pages") |should| equal_to ([4, 10])
-
     def test_merge_all_template_metadata_into_one_dict(self):
         onepage_metatada = self.tccParse._onepage_metadata()
         variouspages_metatada = self.tccParse._variouspages_metadata()
         xml_template_metadata = self.tccParse.xml_template_metadata()
-        xml_template_metadata.keys() |should| equal_to(['grade_postdoctoral', 'title_antecessor', 
-            'author_residue', 'grade_master_degree', 'abstract_antecessor', 'grade_graduation', 
-            'institution_validator', 'pages', 'campus_validator', 'author_position', 'title_sucessor', 
-            'abstract_sucessor', 'author_sucessor', 'grade_spec', 'grade_doctoral', 'page', 'title_breaker'])
+        xml_template_metadata.keys() |should_not| be_empty
 
 
 if __name__ == '__main__':
